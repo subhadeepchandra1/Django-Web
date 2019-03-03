41c9e2cfcd4eb0509374802192e248a366aaa4f1
subhadeepchandra1 <subhadeepchandra1@gmail.com>
2019-03-03 20:41:41 +0530

    Improved Overall Structure of files

--- home/static/home/script.js
+++ home/static/home/script.js
@@ -2,6 +2,16 @@ $(document).ready(function(){
   $(".menu-icon").on("click",function(){
     $("nav ul").toggleClass("showing");
   });
+
+  $(window).on("load", function() {
+    preloaderFadeOutTime = 500;
+    function hidePreloader() {
+      var preloader = $('#wrapper');
+      preloader.fadeOut(preloaderFadeOutTime);
+      }
+      hidePreloader();
+  });
+
 });
 
 $(window).on("scroll",function(){
